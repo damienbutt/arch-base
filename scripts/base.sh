33347c45f7f0c0045421dd7c71f8e7e94a0181dc
Damien Butt <damien.butt@norgateav.com>
2022-05-14 15:46:02 +0100

    refactor(script): add some env vars

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-bash 0-preinstall.sh
-arch-chroot /mnt /arch-base/1-setup.sh
-source /mnt/arch-base/.env
-arch-chroot /mnt /usr/bin/runuser -u ${USERNAME} -- /home/${USERNAME}/arch-base/2-user.sh
-arch-chroot /mnt /arch-base/3-post-setup.sh
-arch-chroot /mnt /bin/bash -c "rm -rf /arch-base/"
+export REPO_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+export REPO_NAME=$(awk -F/ '{print $NF}' <<<${REPO_DIR})
+echo "REPO_DIR=${REPO_DIR}" >>${REPO_DIR}/.env
+echo "REPO_NAME=${REPO_NAME}" >>${REPO_DIR}/.env
+
+bash ${REPO_DIR}/scripts/0-preinstall.sh
+arch-chroot /mnt /${REPO_NAME}/1-setup.sh
+source /mnt/${REPO_NAME}/.env
+arch-chroot /mnt /usr/bin/runuser -u ${USERNAME} -- /home/${USERNAME}/${REPO_NAME}/2-user.sh
+arch-chroot /mnt /${REPO_NAME}/3-post-setup.sh
+arch-chroot /mnt /bin/bash -c "rm -rf /${REPO_NAME}/"
 
 echo "-------------------------------------------------"
 echo "Complete                                         "
